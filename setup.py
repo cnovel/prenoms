6eeff5f18860a2754751b21880dbab25c4b32da6
Cyril Novel <cyril.novel@kosmon.fr>
2020-05-08 15:33:12 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+import prenoms
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name=prenoms.__title__,
+    version=prenoms.__version__,
+    author=prenoms.__author__,
+    author_email='pypi-prenoms@kosmon.fr',
+    url="https://github.com/cnovel/prenoms",
+    description="Générateur de noms aléatoire",
+    long_description='\n\n'.join((
+        readme
+    )),
+    license=prenoms.__license__,
+    packages=find_packages(),
+    package_data={'prenoms': ['dist.*']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'names = prenoms.main:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    test_suite='test_prenoms',
+)
