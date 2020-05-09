0b41c513527a2574f29bd506d24fa99fe79005d5
Cyril Novel <cyril.novel@kosmon.fr>
2020-05-09 17:56:23 +0200

    Fix originality computations

--- prenoms/__init__.py
+++ prenoms/__init__.py
@@ -6,13 +6,13 @@ __author__ = 'Cyril Novel'
 __license__ = 'MIT'
 
 
-def get_prenom(originality: float = 0.5):
+def get_prenom(originality: float = 0.2):
     return prenoms.utils.get_name('first', originality)
 
 
-def get_nom(originality: float = 0.5):
+def get_nom(originality: float = 0.2):
     return prenoms.utils.get_name('last', originality)
 
 
-def get_nom_complet(originality: float = 0.5):
+def get_nom_complet(originality: float = 0.2):
     return '{} {}'.format(get_prenom(originality), get_nom(originality)).strip()
